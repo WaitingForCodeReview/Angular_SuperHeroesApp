a16311618ab88e1dd071065f28b5678d4d196515
Krasnov <roman.krasnov@nure.ua>
2020-09-20 21:31:29 +0300

    US-006: added user info page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {UserCreatePageComponent} from "./user-create-page/user-create-page.compo
 import {MainLayoutComponent} from "./main-layout/main-layout.component";
 import {AuthGuard} from "./auth.guard";
 import {HeroSelectionPageComponent} from "./hero-selection-page/hero-selection-page.component";
+import {UserInfoComponent} from "./user-info-page/user-info/user-info.component";
 
 const routes: Routes = [
   //http://localhost::4200/ --> sign-in page
@@ -14,7 +15,8 @@ const routes: Routes = [
   //http://localhost::4200/create-new-user  --> new user creation page
   {path: 'create-new-user', component: UserCreatePageComponent},
   {path: 'main', component: MainLayoutComponent, canActivate: [AuthGuard], children: [
-      {path: 'hero-selection', component: HeroSelectionPageComponent}
+      {path: 'hero-selection', component: HeroSelectionPageComponent},
+      {path: 'user-info', component: UserInfoComponent}
     ]},
 ];
 
---
+++ src/app/user-info-page/power-ups-tab/power-ups-tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-power-ups-tab',
+  templateUrl: './power-ups-tab.component.html',
+  styleUrls: ['./power-ups-tab.component.css']
+})
+export class PowerUpsTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
